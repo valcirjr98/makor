fd4dbcd8a3542b03b986bdca86bc0f0d75ffdf79
Valcir Junior <valcirjr98@gmail.com>
2018-09-03 14:30:41 -0300

    Activity's para adicionar novos pedidos, clientes e produtos criadas.

---
+++ app/src/main/java/br/com/valcirjr98/makor/AdicionaNovoPedidoActivity.java
@@ -0,0 +1,13 @@
+package br.com.valcirjr98.makor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionaNovoPedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adiciona_novo_pedido);
+    }
+}
